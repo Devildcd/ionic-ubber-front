b9488cb37228cc6d6cba31b19d9b92662445da48
Devildcd <caneladavid19@gmail.com>
2025-06-07 18:54:20 -0500

    Changing the base URL to connect to the API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   googleMapsApiKey: 'AIzaSyB7Gysu0aXgX5RT1AwTgVkOIEjAPsDE0Ik',
-  //baseUrl: 'https://filter-dev-app-backend.onrender.com/api/'
-  baseUrl: 'http://localhost:3000/api/'
+  baseUrl: 'https://filter-dev-app-backend.onrender.com/api/'
+  // baseUrl: 'http://localhost:3000/api/'
 };
